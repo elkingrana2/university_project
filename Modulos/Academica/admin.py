7ad31fd425072562613cadb6a7ef7eddf4586472
elkingrana2 <elkingc1@gmail.com>
2022-11-02 10:04:41 -0500

    Se agrega el modelo carreras

---
+++ Modulos/Academica/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Modulos.Academica.models import Carrera
+
+# Register your models here.
+
+admin.site.register(Carrera)
--- README.md
+++
@@ -1,2 +0,0 @@
-# university_project
-Proyecto Django Para Administrar Una Universidad Aplicación Web con Base de Datos y Envío de Correos
